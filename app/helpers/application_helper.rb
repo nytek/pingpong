fa16ce365d306736f6227b628cee325e7d1b4d65
Nickolas Lanasa <nick@nytekproductions.com>
2014-07-27 19:58:35 -0400

    Added helper method for application controller views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
+# Display value number of stars (*)
 module ApplicationHelper
+  def render_stars(value) 
+    output = ''
+    if (1..5).include?(value)
+      value.times { output += '*'}
+    end
+    output
+  end
 end
